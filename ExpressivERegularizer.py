6835ce06374421345a8a9424ba13f20eb125dfd7
dgail <dominik.gail@gmx.net>
2023-12-14 22:50:47 +0100

    Starts implementation of general composition loss for two body atoms

--- ExpressivERegularizer.py
+++ ExpressivERegularizer.py
@@ -27,11 +27,14 @@ class ExpressivERegularizer(Regularizer):
     ) -> None:
         super().__init__(**kwargs)
 
+        # TODO: Check arguments, if rules_max_body_atoms larger than currently implemented, raise error
+
         # Future Improvement: Move loading to a separate class to allow loading of different formats (AnyBURL, AMIE)
 
         # get dataset and triples factory
         mutable_dataset_kwargs = dict(dataset_kwargs)
         mutable_dataset_kwargs['eager'] = True
+        # TODO: Add apply only once argument to mutable_dataset_kwargs
         dataset = get_dataset(dataset=dataset, dataset_kwargs=mutable_dataset_kwargs)
         # noinspection PyTypeChecker
         self.__factory: TriplesFactory = dataset.training
@@ -53,12 +56,13 @@ class ExpressivERegularizer(Regularizer):
         # add ids
         rule_df['body_ids'] = rule_df['body'].apply(self.__body_ids)
         rule_df['head_id'] = rule_df['head'].apply(self.__head_id)
-        rule_df['ids'] = rule_df.apply(lambda x: x.body_ids.union([x.head_id]), axis=1)
+        rule_df['ids'] = rule_df.apply(lambda x: set(x.body_ids).union([x.head_id]), axis=1)
 
         self.__rules = rule_df
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
-        return torch.FloatTensor([0])
+        rule_loss = self.__rules.apply(lambda row: self.__compute_loss(row, x), axis=1)
+        return torch.FloatTensor([rule_loss.sum()])
 
     def __no_const_body(self, atoms: [str]) -> bool:
         arguments = map(self.__extract_arguments, atoms)
@@ -82,10 +86,9 @@ class ExpressivERegularizer(Regularizer):
         regex_result = pattern.match(arguments)
         return regex_result is not None
 
-    def __body_ids(self, atoms: [str]) -> set:
+    def __body_ids(self, atoms: [str]) -> [int]:
         relations = list(map(self.__extract_relation, atoms))
-        ids = self.__factory.relations_to_ids(relations)
-        return set(ids)
+        return self.__factory.relations_to_ids(relations)
 
     def __head_id(self, atom) -> int:
         relation = self.__extract_relation(atom)
@@ -99,5 +102,105 @@ class ExpressivERegularizer(Regularizer):
 
         return relation
 
+    def __compute_loss(self, rule, weights) -> float:
+        body_args = map(self.__extract_arguments, rule['body'])
+        body_args = list(map(lambda args: args[1:-1], body_args))
+        head_args = self.__extract_arguments(rule['head'])
+        head_args = head_args[1:-1]
+
+        # TODO: Implement body_count == 0
+
+        if rule['body_count'] == 1:
+            return self.__compute_loss_one_atom(body_args, head_args, rule['body_ids'], rule['head_id'], weights)
+        elif rule['body_count'] == 2:
+            return self.__compute_loss_two_atoms(body_args, head_args, rule['body_ids'], rule['head_id'], weights)
+
+        return 0
+
+    # TODO: Move to separate class
+    def __compute_loss_one_atom(self, body_args, head_args, body_ids, head_id, weights) -> float:
+        if head_args == 'X,Y':
+            # hierarchy: r(x,y) -> s(x,y) = r(x,y) and i(y,y) -> s(x,y)
+            # TODO: Add computation
+            return 0
+        else:
+            # inversion: r(x,y) -> r(y,x) = r(x,y) and i(y,y) -> r(y,x)
+            print("Loss for inversion rules not implemented yet")
+            return 0
+
+    def __compute_loss_two_atoms(self, body_args, head_args, body_ids, head_id, weights) -> float:
+        chain_order = self.__compute_chain_order(body_args, [], 'X')
+
+        if all(chain_order) and head_args == 'X,Y':
+            body_indices = torch.tensor(body_ids)
+            body_weights = torch.index_select(weights, 0, body_indices)
+            head_weights = weights[head_id, :]
+            head_weights = torch.reshape(head_weights, (1, 6))
+            rule_weights = torch.cat((body_weights, head_weights), dim=0)
+            return self.general_composition_loss(rule_weights)
+        else:
+            print("Loss function only for chained, connected rules without inverted variables implemented.")
+            return 0
+
+    def __compute_chain_order(self, body_args, current_chain, prev_dangling_atom) -> [bool]:
+        """
+        Determines for which relations the order of the variables is reversed - e.g. A,X instead of X,A .
+        Applied recursively.
+
+        :param body_args: List of arguments of the different body arguments - e.g. ["X,A","B,A","B,Y"]
+        :param current_chain: List of current order in previous recursion.
+        :param prev_dangling_atom: Dangling atom in previous relation (atom). (Recursion!)
+        :return: A list of which relations have the order of variables reversed.
+        """
+
+        if len(body_args) == 1:
+            ordered = body_args[0][0] == prev_dangling_atom
+            return current_chain + [ordered]
+        else:
+            if body_args[0][0] == prev_dangling_atom:
+                # New variable on right side
+                return self.__compute_chain_order(body_args[1:], current_chain + [True], body_args[0][2])
+            else:
+                # New variable on left side
+                return self.__compute_chain_order(body_args[1:], current_chain + [False], body_args[0][2])
+
+    def general_composition_loss(self, weights) -> float:
+        """
+        Computes the general composition loss (for two body atoms)
+
+        :param weights: Weights for all three relations (two body relations, one head relation)
+        :return: The loss for general composition
+        """
+
+        d1_h, d1_t, c1_h, c1_t, s1_h, s1_t = weights[0, :]
+        d2_h, d2_t, c2_h, c2_t, s2_h, s2_t = weights[1, :]
+        d3_h, d3_t, c3_h, c3_t, s3_h, s3_t = weights[2, :]
+
+        # Calculate four corner points of head relation
+        corner1_x = ((c3_t-c3_h) + (d3_t-d3_h)) / (s3_t-s3_h) # top right
+        corner1_y = c3_t + d3_t + s3_h * corner1_x
+
+        corner2_x = ((c3_t-c3_h) + (-d3_t-d3_h)) / (s3_t-s3_h) # bottom right
+        corner2_y = c3_t - d3_t + s3_h * corner2_x
+
+        corner3_x = ((c3_t - c3_h) + (-d3_t + d3_h)) / (s3_t - s3_h)  # bottom left
+        corner3_y = c3_t - d3_t + s3_h * corner3_x
+
+        corner4_x = ((c3_t - c3_h) + (d3_t + d3_h)) / (s3_t - s3_h)  # top left
+        corner4_y = c3_t + d3_t + s3_h * corner4_x
+
+        # Check each inequality with each corner point
+
+        # Return sum of max mismatch for each corner
+
+        return 0
+
+
 if __name__ == '__main__':
     reg = ExpressivERegularizer("WN18RR", dataset_kwargs={}, rules="../Rules/WN18RR-Max5-Run1/WN18RR-1000", rules_max_body_atoms=3, rule_min_confidence=0.1)
+    reg.forward(torch.FloatTensor([[0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],
+                                   [0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],
+                                   [0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],
+                                   [0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],
+                                   [0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],
+                                   [0.1, 0.2, 0.1, 0.3, 0.1, 0.4], [0.1, 0.5, 0.1, 0.6, 0.1, 0.7], [0.1, 0.8, 0.1, 0.9, 0.1, 0.10],]))
